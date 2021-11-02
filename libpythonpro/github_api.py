647c5aba3de166e720a9aed64ebe05e39c749c90
Henrique-Marmiroli <lhm.ambiental@gmail.com>
2021-11-01 21:03:19 -0300

    Correção requirements.

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -2,12 +2,11 @@ import requests
 
 
 def buscar_avatar(usuario):
-    '''
+    """
     Busca um avatar de um usuario no github.
     :param usuario: str com o nome de usuario no github
     :return: str com link do avatar
-    '''
-
+    """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
@@ -15,4 +14,3 @@ def buscar_avatar(usuario):
 
 if __name__ == '__main__':
     print(buscar_avatar('henrique-marmiroli'))
-
