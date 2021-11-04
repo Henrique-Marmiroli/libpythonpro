f56eeb7652b26a11e16816fd9c1599dae79d0e03
Henrique-Marmiroli <lhm.ambiental@gmail.com>
2021-11-04 17:19:54 -0300

    Concluído testes automáticos.

--- libpythonpro/tests/tests_spam/test_github_api.py
+++ libpythonpro/tests/tests_spam/test_github_api.py
@@ -1,14 +1,26 @@
 from unittest.mock import Mock
-
+import pytest
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/128897?v=4'
     resp_mock.json.return_value = {
         'login': 'henrique', 'id': 128897,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/128897?v=4'
+        'avatar_url': url
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('henrique')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('henrique')
     assert 'https://avatars.githubusercontent.com/u/128897?v=4' == url
